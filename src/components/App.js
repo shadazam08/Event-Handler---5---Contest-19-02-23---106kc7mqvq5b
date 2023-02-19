b27b66a14dc0aabcec984d0df5e8577cd808c532
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-02-19 13:50:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const buttonClick = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+  }
 
   return (
     <div id="main">
-      <form onSubmit = "console.log('form submited'); return flase">
+      <form>
         <label htmlFor='name'>Name</label>
         <input id="name" type={"text"} />
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={buttonClick}>Submit</button>
       </form>
     </div>
   )
